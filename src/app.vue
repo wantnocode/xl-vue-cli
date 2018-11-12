a5a3f8efe3565756d786d587c93ff64d3cdd8d94
xgl <17610017985@163.com>
2018-11-12 17:29:23 +0800

    update

---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <p class="test">111112222111111111111111111111test text</p>
+        <p>{{msg}}</p>
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+    export default {
+        name:'app',
+        data(){
+            return {
+                msg:"hello vue !!"
+            }
+        },
+    }
+</script>
+<style >
+    .test{
+        color:#020202;
+        font-size:18px;
+    }
+</style>
